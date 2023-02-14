7d52073dc3523dc0a4c356c407db152ea00670c2
Serhii Tereshchenko <greywolf1396@gmail.com>
2023-02-15 00:37:17 +0200

    json

--- SilentApp.FunctionsApp/Dto/SensorReading.cs
+++ SilentApp.FunctionsApp/Dto/SensorReading.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using System.Text.Json.Serialization;
 
 namespace SilentApp.FunctionsApp.Dto
 {
     public class SensorReading
     {
+        [JsonPropertyName("deviceName")]
         public string DeviceName { get; set; }
+
+        [JsonPropertyName("temperature")]
         public decimal Temperature { get; set; }
+
+        [JsonPropertyName("humidity")]
         public decimal Humidity { get; set; }
     }
 }
